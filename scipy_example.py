ed69f3838aa14db6b002e7af23a53e2dd12e9942
andreabassi78 <andreabassi@polimi.it>
2025-01-23 22:58:56 +0100

    cv2

---
+++ scipy_example.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+from scipy import ndimage, datasets
+
+data=datasets.ascent()
+
+sigma = 2
+data = ndimage.gaussian_filter(data,sigma)
+
+zoom = 0.5        
+data = ndimage.zoom(data, zoom) 
+shape = data.shape
+
+print(shape)
+
+
+plt.imshow(data)
+plt.colorbar()
+plt.show()
