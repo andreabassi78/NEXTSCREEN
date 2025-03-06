c8d9806383f493e05c4a1b74c82efe6ce0a4bf57
andreabassi78 <andreabassi@polimi.it>
2025-03-06 09:53:22 +0100

    face recognition opencv added

--- python_basics/open_cv_example.py
+++ python_basics/open_cv_example.py
@@ -1,3 +1,9 @@
+"""
+Face recognition within an image    
+
+For description of cascade classifiers read https://docs.opencv.org/4.x/db/d28/tutorial_cascade_classifier.html
+"""
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,14 +18,15 @@ image = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
 # Load the pre-trained Haar Cascade classifier for face detection
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Load the pre-trained Haar Cascade classifier for face detection
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
 # Convert the image to grayscale (Haar cascades require grayscale images)
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Perform face detection
 faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+# scalefactor 1.1 : resizes the image in each iteration to find faces of different sizes
+# minNeighbors = 5 minimum number of detected neighbors rectangles to confirm that a face was detected
+# minSize = (30,30) minimum size of the object to be detected  
+
 
 # Draw rectangles around detected faces
 for (x, y, w, h) in faces:
