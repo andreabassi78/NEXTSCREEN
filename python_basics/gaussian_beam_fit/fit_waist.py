f34a5453477e6081ce80a139a0033d27dbea2358
andreabassi78 <andreabassi@polimi.it>
2025-03-18 17:59:40 +0100

    guassian beam fit

---
+++ python_basics/gaussian_beam_fit/fit_waist.py
@@ -0,0 +1,95 @@
+import numpy as np
+import tifffile     
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from scipy.interpolate import UnivariateSpline
+from tkinter import filedialog     
+import os  
+
+filename = 'beam_2_5_x.tiff'
+
+camera_pixel = 4.8 #um
+M = 2.5 # magnification
+wavelenght = 0.473 #um
+n = 1
+
+pixel_size = camera_pixel/M
+
+
+full_path = os.path.realpath(__file__)
+folder, _ = os.path.split(full_path) 
+file_path = os.path.join(folder,filename)
+
+image = tifffile.imread(file_path)
+
+def gaussian(x, A, x0, w, offset):
+    return A * np.exp(-((x - x0) ** 2) / (w ** 2)) + offset
+
+
+
+num_rows, num_cols = image.shape
+
+fwhm_values = np.zeros(num_cols)
+
+for col in range(num_cols):    
+
+    profile = image[:, col]      
+    y = np.arange(num_rows) 
+    # initial parameter
+    amplitude = np.max(profile) - np.min(profile) 
+    center = np.amax(y)/2                    
+    w0 = np.amax(y) / 50                 
+    offsets = np.min(profile)                     
+    p0 = [amplitude, center, w0, offsets]
+
+    popt, _ = curve_fit(gaussian, y, profile, p0=p0)     
+    _, _, w_fit, _ = popt   
+    fwhm = 2.355/2 * w_fit   
+    fwhm_values[col] = fwhm     
+
+    if col==num_cols//2:
+        plt.figure()
+        plt.plot(y,profile, color='m')
+        plt.plot(y,gaussian(y,*popt), color='k')
+
+x = np.arange(num_cols) 
+spl = UnivariateSpline(x, fwhm_values, s=100)
+fitted_fwhm_values = spl(x)
+
+#find the minimum FWHM value and its index
+min_index = np.argmin(fitted_fwhm_values)
+min_fwhm = fitted_fwhm_values[min_index]
+min_w = min_fwhm *2/2.355 
+
+zr = np.pi* min_w**2/wavelenght*n
+w_zr = (np.sqrt(2))* (min_fwhm)
+
+left_side = fitted_fwhm_values[:min_index]
+right_side = fitted_fwhm_values[min_index:] 
+
+closest_left = np.argmin(np.abs(left_side - w_zr))
+closest_left_value = fitted_fwhm_values[closest_left]
+zr_left = (-closest_left+min_index)*pixel_size
+
+closest_right = np.argmin(np.abs(right_side - w_zr)) + min_index
+closest_right_value = fitted_fwhm_values[closest_right]
+zr_right = (closest_right-min_index)*pixel_size
+
+
+print(f"The minimum FWHM value is {min_fwhm:.2f} um, the waist is {min_w:.2f} um, at column {min_index}")
+print(f"Teoretical Rayleight distance {zr:.2f} um: FWHM: {w_zr:.2f}")
+print(f"Rayleigh distance (left): {zr_left:.2f} um, FWHM: {closest_left_value:.2f} um")
+print(f"Rayleigh distance (right): {zr_right:.2f} um, FWHM: {closest_right_value:.2f} um")
+
+plt.figure()
+plt.plot(fwhm_values, color='m')
+plt.plot(spl(x))
+
+plt.figure()
+plt.imshow(image, cmap='gray', aspect='auto')
+plt.axvline(x=min_index, color='c', linestyle='--', label='Min FWHM')
+plt.axvline(x=closest_left, color='m', linestyle='--', label='Closest Left')
+plt.axvline(x=closest_right, color='m', linestyle='--', label='Closest Right')
+plt.legend()
+
+plt.show()
